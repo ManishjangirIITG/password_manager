7fcc4cc73d7005737531ccbe2180beac3009d9fd
ManishjangirIITG <m.jangir@iitg.ac.in>
2024-06-20 18:35:18 +0530

    updated the package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs/promises';
+import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config
+
 export default defineConfig({
-  base: '/password_manager/',
+  base: "/password_manager/",
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
 })
