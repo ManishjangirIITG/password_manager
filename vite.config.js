fc90a8610386ebbbaefedc6bde9ea059275fc388
ManishjangirIITG <m.jangir@iitg.ac.in>
2024-06-20 12:26:51 +0530

    Added server->fs->strict=false in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/password_manager/',
-  plugins: [react()], 
+  plugins: [react()],
+  server: {
+    fs: {
+      strict: false
+    }
+  }
 })
